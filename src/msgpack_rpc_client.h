8d3dcdf9ea9a7542bc1c040145dda6c23d718055
Toshinori Sato <overlasting@gmail.com>
2014-10-17 14:24:42 +0900

    replase mpc => mrc

--- src/msgpack_rpc_client.h
+++ src/msgpack_rpc_client.h
@@ -1,47 +1,47 @@
-#ifndef __MSGPACK_CLIENT_C_WRAPPER_H
-#define __MSGPACK_CLIENT_C_WRAPPER_H
+#ifndef __MSGPACK_RPC_CLIENT_C_WRAPPER_H
+#define __MSGPACK_RPC_CLIENT_C_WRAPPER_H
 
-typedef void mpclient;
+typedef void mrclient;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-  mpclient*   mpc_create(char *, int);
-  void        mpc_destroy(mpclient *);
-  const char* mpc_call(mpclient *, char *);
-  const char* mpc_call(mpclient *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_call(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *);
-  const char* mpc_notify(mpclient *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
-  const char* mpc_notify(mpclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  mrclient*   mrc_create(char *, int);
+  void        mrc_destroy(mrclient *);
+  const char* mrc_call(mrclient *, char *);
+  const char* mrc_call(mrclient *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_call(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *);
+  const char* mrc_notify(mrclient *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
+  const char* mrc_notify(mrclient *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *, char *);
 #ifdef __cplusplus
 }
 #endif
