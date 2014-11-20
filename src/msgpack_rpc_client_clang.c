e3a8e7935d836820b2d89e1f44bead786ad64101
Toshinori Sato <overlasting@gmail.com>
2014-11-20 18:12:10 +0900

    Fix sample code of msgpack_rpc_client in C

--- src/msgpack_rpc_client_clang.c
+++ src/msgpack_rpc_client_clang.c
@@ -1,10 +1,23 @@
 #include "msgpack_rpc_client.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void) {
   mrclient* client = mrc_create("127.0.0.1", 9090);
-  const char *result = mrc_call(client, "distance", "タモリ");
-  printf("%s\n", result);
+  char query[200];
+  const char *result = NULL;
+  while (1) {
+    printf("please input query >");
+    scanf("%s", query);
+    if (strcmp(query, "q")) {
+    } else {
+      break;
+    }
+    printf("\n query = %s\n", query);
+    result = (const char *)mrc_call(client, "distance", query);
+    printf("%s\n", result);
+  }
   mrc_destroy(client);
   return 1;
 }
